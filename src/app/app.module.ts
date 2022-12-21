ea1cdf7d993944e8dfae76722015761f0ace410d
pipe <felipe.parra@blackmind.cl>
2022-12-20 23:41:02 -0300

    problemas con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import firebaseConfig from './firebase';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    // AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireAuthModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-registro',
@@ -8,28 +9,50 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  username : string = "";
+  password : string = "";
+  cpassword: string = "";
 
   constructor(public alertController: AlertController, private router:Router) { }
 
   ngOnInit() {
   }
 
-  async presentAlertConfirm() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Registro',
-      message: 'Se ha registrado exitosamente',
-      buttons: [
-        {
-          text: 'Ok',
-          id: 'confirm-button',
-          handler: () => {
-            this.router.navigateByUrl('/login');
-          }
-        }
-      ]
+  async registrar() {
+    const {username, password, cpassword} = this
+    if(password !== cpassword){
+      this.showAlert("Error!", "Las contraseñas no coinciden");
+    }
+    try{
+      // const res = await 
+      this.showAlert("Usuario Registrado", "Bienvenido "+username)
+      this.route.navigate(['login'])
+    } catch(err){
+      console.dir(err);
+      this.showAlert("Error", err.message);
+    }
+    // const alert = await this.alertController.create({
+    //   cssClass: 'my-custom-class',
+    //   header: 'Registro',
+    //   message: 'Se ha registrado exitosamente',
+    //   buttons: [
+    //     {
+    //       text: 'Ok',
+    //       id: 'confirm-button',
+    //       handler: () => {
+    //         this.router.navigateByUrl('/login');
+    //       }
+    //     }
+    //   ]
+    // });
+    // await alert.present();
+  }
+  async showAlert(header:string,message:string){
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ["ok"]
     });
-
     await alert.present();
   }
 
